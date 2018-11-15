54acd7e7dde08887cb5e1956045bd3092ca247c2
nkv16786 <nkv16786@gmail.com>
2018-11-15 14:55:00 +0530

    Add files via upload

---
+++ get-cpu.ps1
@@ -0,0 +1,19 @@
+﻿try{
+$usage= Get-WmiObject win32_processor -computername localhost | select DeviceID,LoadPercentage -ErrorAction Stop
+$count = 1
+$cpu_usage = @{}
+foreach ($value in $usage){
+$cpu_usage.Add($usage[$count-1].DeviceID, $usage[$count-1].LoadPercentage)
+$count = $count+1
+}
+$output = @{"output" = $cpu_usage; "additional_attributes" = $cpu_usage}
+return $output | convertto-json
+
+}
+catch{
+$Errmsg = $_.Exception.Message
+$err = "Command Failed:"+$Errmsg
+$err1 = @{"Error" = $err}
+return $err1 | ConvertTo-Json
+
+}
